3c21b0fe0da2d21eafebcac91385736398166939
David Doan <daviddoan@Davids-MacBook-Pro.local>
2024-07-18 13:04:57 -0600

    Solution

--- lib/debug.py
+++ lib/debug.py
@@ -6,6 +6,39 @@ from classes.many_to_many import Visitor
 from classes.many_to_many import Trip
 
 if __name__ == '__main__':
-    print("HELLO! :) let's debug :vibing_potato:")
+    v1 = Visitor(name = "Bob")
+    v2 = Visitor(name = "Grey")
+    # v1.name = "New Visitor"
+    # print(v1.name)
 
-    ipdb.set_trace()
+    p1 = NationalPark(name="Rocky Mountains")
+    p2 = NationalPark(name="Yosemite")
+    # p1.name = "new park"
+
+    Trip(
+        visitor=v1,
+        national_park=p1,
+        start_date="May 1st",
+        end_date="May 2nd"
+    )
+    Trip(
+        visitor=v1,
+        national_park=p1,
+        start_date="May 5st",
+        end_date="May 7nd"
+    )
+    Trip(
+        visitor=v2,
+        national_park=p1,
+        start_date="May 5st",
+        end_date="May 7nd"
+    )
+    Trip(
+        visitor=v1,
+        national_park=p2,
+        start_date="May 5st",
+        end_date="May 7nd"
+    )
+
+    print(v2.total_visits_at_park(p2))
+    
